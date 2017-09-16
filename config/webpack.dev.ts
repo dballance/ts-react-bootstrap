d37d8315743346ff7437b038233986d52b38fd32
Drew Ballance <drew.ballance@hexagon.com>
2017-09-16 11:30:02 -0500

    Add prod config and move towards hot reload

--- config/webpack.dev.ts
+++ config/webpack.dev.ts
@@ -7,15 +7,48 @@ import { getWebpackStatsConfig } from './utils';
 
 const config = webpackMerge(
   commonConfig, 
+  
   {
-    devtool: 'source-map', 
+    entry : {
+      app: [
+        "react-hot-loader/patch",
+        './src/main.ts'
+      ],
+      vendor:  [
+        "react-hot-loader/patch",'./src/vendor.ts',
+      ],
+      polyfill:  [
+        "react-hot-loader/patch",'./src/polyfills.ts'
+      ],
+    }, 
+    devtool: 'eval', 
     devServer: {
       contentBase: path.resolve(__dirname, '../dist'), 
       compress: true, 
       port: 3000,
       overlay: true, 
+      hot: true,
       stats: getWebpackStatsConfig(false)
-    }
+    }, 
+    module: {
+      rules: [
+        {
+          test: /\.ts$/, 
+          use: [
+            "react-hot-loader/webpack",
+            {
+              loader:'ts-loader',
+              options: {
+                configFile: 'tsconfig.json'
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
   }
 )
 
---
+++ config/webpack.prod.ts
@@ -0,0 +1,24 @@
+const path = require('path');
+import * as webpack from 'webpack'; 
+import * as webpackMerge from 'webpack-merge';
+import commonConfig from './webpack.common';
+import { getWebpackStatsConfig } from './utils';
+
+
+const config = webpackMerge(
+  commonConfig, 
+  {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      new webpack.optimize.UglifyJsPlugin(<any>{
+        mangle: { screw_ie8: true },
+        compress: { screw_ie8: true },
+        sourceMap: true
+      })
+    ]
+  }
+)
+
+export default config;
