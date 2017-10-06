6e6d9820a25d909c9338c7612606f4aa3fddd977
Drew Ballance <drew.ballance@hexagon.com>
2017-10-06 16:20:39 -0500

    add test config

---
+++ src/app/app.spec.tsx
@@ -0,0 +1,19 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+
+import App from './app'
+
+describe('App ', () => {
+  it('should render without throwing', () => {
+    const wrapper = shallow(<App toWhat="Foo" />)
+
+    expect(wrapper.find('div').length).toBe(1)
+    expect(wrapper.find('div').hasClass('title')).toBe(true)
+  })
+
+  it('should render text', () => {
+    const wrapper = shallow(<App toWhat="Foo" />)
+
+    expect(wrapper.find('div').text()).toBe('Hello ! Foo')
+  })
+})
